7338e0de189ced3622138ec0782961f9c722f330
Ula Wang <lilingwang32@gmail.com>
2024-06-04 23:32:38 +0800

    add:.gitignore

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoCreateView
+from .views import TodoCreateView,TodoListView
 
 app_name = "todos"
 
 urlpatterns = [
     path("create/",TodoCreateView.as_view(),name ="create"),
+    path("list/",TodoListView.as_view(),name ="list"),
 ]

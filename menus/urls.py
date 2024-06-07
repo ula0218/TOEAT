317dfe865a7a56093511b0af6374f30051a1d139
Ula Wang <lilingwang32@gmail.com>
2024-06-07 14:33:19 +0800

    add:map

--- menus/urls.py
+++ menus/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CategorySelectionView
+from .views import CategorySelectionView,MapView
 
 app_name='menus'
 
 urlpatterns = [
     path('create/', CategorySelectionView.as_view(), name='create'),
+    path('map/', MapView.as_view(), name='map'),
 ]
--- menus/views.py
+++ menus/views.py
@@ -3,6 +3,10 @@ from .forms import MenuForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Menu
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import View
+from django.views.generic import TemplateView
 
 
 class CategorySelectionView(LoginRequiredMixin, CreateView):
@@ -14,3 +18,8 @@ class CategorySelectionView(LoginRequiredMixin, CreateView):
         user_id = self.request.user.id
         form.instance.user_id = user_id
         return super().form_valid(form)
+    
+class MapView(View):
+    def get(self, request):
+        google_api_key = settings.GOOGLE_MAPS_API_KEY
+        return render(request, "menus/maps.html", {"google_api_key": google_api_key})

2c5a32f1ccc3ef0111ce6562bb1d5b39a8b8961a
Ula Wang <lilingwang32@gmail.com>
2024-06-11 17:19:51 +0800

    add:plotly and TodoStatsView

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TodoCreateView,TodoListView
+from .views import TodoCreateView,TodoListView,TodoStatsView
 
 app_name = "todos"
 
 urlpatterns = [
     path("create/",TodoCreateView.as_view(),name ="create"),
     path("list/",TodoListView.as_view(),name ="list"),
+    path('chart/', TodoStatsView.as_view(), name='chart'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,16 @@
-# views.py
 from datetime import datetime
-from django.http import JsonResponse
-from django.views import View
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Todo
 from .forms import TodoForm
+import plotly.graph_objs as go
+from django.db.models import Count
+from django.db.models.functions import TruncDate
+from plotly.io import to_json
+
 
 class TodoCreateView(LoginRequiredMixin, CreateView):
     model = Todo
@@ -55,4 +58,50 @@ class TodoListView(ListView):
             grouped_todos[date].append(todo)
         context['grouped_todos'] = grouped_todos
         context['filter_applied'] = self.request.GET.get('startDate') and self.request.GET.get('endDate')
+        return context
+
+class TodoStatsView(TemplateView):
+    template_name = 'todos/chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # 日期
+        date_counts = Todo.objects.annotate(date_annotated=TruncDate('date')).values('date_annotated').annotate(total=Count('id')).order_by('date_annotated')
+        dates = [item['date_annotated'].strftime('%Y/%m/%d') for item in date_counts]
+        date_totals = [item['total'] for item in date_counts]
+
+        # 每日飲食內容紀錄
+        date_details = {}
+        for item in date_counts:
+            date_str = item['date_annotated'].strftime('%Y/%m/%d')
+            records = Todo.objects.filter(date=item['date_annotated'])
+            date_details[date_str] = [{'food': record.food, 'type': dict(Todo.TYPES_CHOICES)[record.type],'hungry': dict(Todo.HUNGRY_CHOICES)[record.hungry]} for record in records]
+
+        # 圖表對象
+        date_chart = go.Bar(
+            y=dates,
+            x=date_totals,
+            orientation='h',
+            name='日期',
+            marker=dict(color='rgb(255, 192, 203)', line=dict(color='rgb(255, 105, 180)', width=1.5)),
+            customdata=[date_details[date] for date in dates],
+            hovertemplate='%{y}<br>次數: %{x}<extra></extra>'
+        )       
+
+        # 圖表佈局
+        layout = go.Layout(
+            xaxis={'title': '單日用餐次數'},
+            yaxis={'title': '日期'},
+            width=800,
+            height=600,
+            plot_bgcolor='rgb(230, 230, 250)',
+        )
+
+        fig = go.Figure(data=[date_chart], layout=layout)
+
+        # 圖表轉換成 JSON 
+        chart_json = to_json(fig)
+
+        context['chart_json'] = chart_json
         return context
